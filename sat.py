07b73e2084a1d652ed04702ea65b7fb955aebf20
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2016-05-30 13:26:39 +1000

    tutor implementation

--- sat.py
+++ sat.py
@@ -161,14 +161,12 @@ def PmfProbGreater(pmf1, pmf2):
         float probability
     """
     total = 0.0
-    runsum = 0.0
     for x1, p1 in pmf1.Items():
         for x2, p2 in pmf2.Items():
-            if p1 < p2:
-                total += 1
-            runsum += 1
+            if x1 > x2:
+                total += p1 * p2
 
-    return total/runsum
+    return total
 
 
 def main():
